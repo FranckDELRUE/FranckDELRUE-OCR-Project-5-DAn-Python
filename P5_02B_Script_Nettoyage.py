054910f1e2c53eb030025613838254e878e4e018
FranckDELRUE <delrue.franck@gmail.com>
2020-08-11 09:22:54 +0200

    First Commit

---
+++ P5_02B_Script_Nettoyage.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import P5_03_OCR5 as OCR
+
+df_data_raw = pd.read_csv('./Data/data_raw.csv',encoding = "ISO-8859-1")
+
+# Nous enlevons les données concernant des groupements de pays (Code zone > 5000)
+df_data = df_data_raw[~(df_data_raw['Code zone'] >= 5000)]
+
+# Nous enlevons les données concernant la Chine qui est un groupement de pays (Code Zone = 351)
+df_data = df_data[~(df_data['Code zone'] == 351)]
+
+# Nous gardons que les lignes qui nous intéressent
+codeElementAGarder = [511, 664, 674]
+codeProduitAGarder = [2501, 2901, 2941]
+
+df_data = df_data[df_data['Code Produit'].isin(codeProduitAGarder)]
+df_data = df_data[df_data['Code Élément'].isin(codeElementAGarder)]
+
+# Nous calculons la différence de populations en %
+df_data['Difference de population'] = (df_data['Y2017'] - df_data['Y2014']) / df_data['Y2017'] * 100
+
+# Nous mettons en colonne les lignes du tableau
+df_data = OCR.ligneToColonne(df_data, [2901, 664, 'Disponibilité alimentaire Kcal'])
+df_data = OCR.ligneToColonne(df_data, [2901, 674, 'Disponibilité alimentaire Protéine'])
+df_data = OCR.ligneToColonne(df_data, [2941, 674, 'Proportion Protéine Animale'])
+
+# Nous calculons la proportion de protéine animale par rapport au protéine totale
+df_data['Proportion Protéine Animale'] /= df_data['Disponibilité alimentaire Protéine']
+
+# Nous enlevons les lignes qui ne nous servent plus
+filt = df_data['Code Élément'] == 511
+colonne = ['Zone', 'Difference de population', 'Disponibilité alimentaire Kcal', 'Disponibilité alimentaire Protéine', 'Proportion Protéine Animale']
+df_data = df_data.loc[filt, colonne]
+
+# Nous exportons le fichier nettoyé
+df_data.to_csv(path_or_buf='./Export/data.csv', index=False)
+
+
---
+++ P5_03_OCR5.py
@@ -0,0 +1,189 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from scipy.cluster.hierarchy import dendrogram
+from matplotlib.collections import LineCollection
+
+
+def plot_dendrogram(Z, names):
+    plt.figure(figsize=(10,25))
+    plt.title('Hierarchical Clustering Dendrogram')
+    plt.xlabel('distance')
+    dendrogram(
+        Z,
+        labels = names,
+        orientation = "left",
+    )
+    plt.show()
+    
+def ligneToColonne(df, liste):
+    """
+        Prends une data frame et une liste. Permet de mettre des éléments du dictionnaire, présentés en ligne, en colonne.
+        Retourne la data Frame avec les colonnes.
+        Liste[0] est le Code Produit
+        Liste[1] est le Code Élément
+        Liste[2] est le nom de la nouvelle colonne
+    """
+    
+    filt = (df['Code Produit'] == liste[0]) & (df['Code Élément'] == liste[1])
+    colonne = ['Zone', 'Y2017']
+    avec = ['Zone']
+
+    df = df.merge(df.loc[filt, colonne], how = 'left', on = avec).fillna(0)
+    df.rename(columns={'Y2017_x': 'Y2017', 'Y2017_y': liste[2]}, inplace=True)
+    
+    return df
+
+def display_scree_plot(pca):
+    scree = pca.explained_variance_ratio_*100
+    plt.bar(np.arange(len(scree))+1, scree)
+    plt.plot(np.arange(len(scree))+1, scree.cumsum(),c="red",marker='o')
+    plt.xlabel("rang de l'axe d'inertie")
+    plt.ylabel("pourcentage d'inertie")
+    plt.title("Eboulis des valeurs propres")
+    plt.show(block=False)
+    
+def display_circles(pcs, n_comp, pca, axis_ranks, labels=None, label_rotation=0, lims=None):
+    for d1, d2 in axis_ranks: 
+        if d2 < n_comp:
+
+            # initialisation de la figure
+            fig, ax = plt.subplots(figsize=(20,20))
+
+            # détermination des limites du graphique
+            if lims is not None :
+                xmin, xmax, ymin, ymax = lims
+            elif pcs.shape[1] < 30 :
+                xmin, xmax, ymin, ymax = -1, 1, -1, 1
+            else :
+                xmin, xmax, ymin, ymax = min(pcs[d1,:]), max(pcs[d1,:]), min(pcs[d2,:]), max(pcs[d2,:])
+
+            # affichage des flèches
+            # s'il y a plus de 30 flèches, on n'affiche pas le triangle à leur extrémité
+            if pcs.shape[1] < 30 :
+                plt.quiver(np.zeros(pcs.shape[1]), np.zeros(pcs.shape[1]),
+                   pcs[d1,:], pcs[d2,:], 
+                   angles='xy', scale_units='xy', scale=1, color="grey")
+                # (voir la doc : https://matplotlib.org/api/_as_gen/matplotlib.pyplot.quiver.html)
+            else:
+                lines = [[[0,0],[x,y]] for x,y in pcs[[d1,d2]].T]
+                ax.add_collection(LineCollection(lines, axes=ax, alpha=.1, color='black'))
+            
+            # affichage des noms des variables  
+            if labels is not None:  
+                for i,(x, y) in enumerate(pcs[[d1,d2]].T):
+                    if x >= xmin and x <= xmax and y >= ymin and y <= ymax :
+                        plt.text(x, y, labels[i], fontsize='14', ha='center', va='center', rotation=label_rotation, color="blue", alpha=0.5)
+            
+            # affichage du cercle
+            circle = plt.Circle((0,0), 1, facecolor='none', edgecolor='b')
+            plt.gca().add_artist(circle)
+
+            # définition des limites du graphique
+            plt.xlim(xmin, xmax)
+            plt.ylim(ymin, ymax)
+        
+            # affichage des lignes horizontales et verticales
+            plt.plot([-1, 1], [0, 0], color='grey', ls='--')
+            plt.plot([0, 0], [-1, 1], color='grey', ls='--')
+
+            # nom des axes, avec le pourcentage d'inertie expliqué
+            plt.xlabel('F{} ({}%)'.format(d1+1, round(100*pca.explained_variance_ratio_[d1],1)))
+            plt.ylabel('F{} ({}%)'.format(d2+1, round(100*pca.explained_variance_ratio_[d2],1)))
+
+            plt.title("Cercle des corrélations (F{} et F{})".format(d1+1, d2+1))
+            plt.show(block=False)
+            
+def display_factorial_planes(X_projected, n_comp, pca, axis_ranks, data, labels=None, alpha=1, illustrative_var=None):
+    for d1,d2 in axis_ranks:
+        if d2 < n_comp:
+ 
+            # initialisation de la figure       
+            fig = plt.figure(figsize=(20,20))
+        
+            color1=(0, 0, 1, 1)
+            color2=(1, 0.65, 0, 1)
+            color3=(0, 1, 0, 1)
+            color4=(1, 0, 0, 1)
+            color5=(0.29, 0, 0.51, 1)
+
+            colormap = np.array([color1, color2, color3, color4, color5])
+            
+            X_projected = np.hstack((X_projected, np.atleast_2d(data).T))
+
+            # affichage des points
+            if illustrative_var is None:
+                plt.scatter(X_projected[:, d1], X_projected[:, d2], alpha=alpha, c = colormap[data-1])
+                meanD1 = 0
+                meanD2 = 0
+                for i in range(0,int(data.max())):
+                    meanD1 = X_projected[X_projected[ : , -1] == i+1][:, d1].mean()
+                    meanD2 = X_projected[X_projected[ : , -1] == i+1][:, d2].mean()
+                    plt.scatter(meanD1, meanD2, marker = '^', s = 200, alpha=alpha, c = colormap[i])
+            else:
+                illustrative_var = np.array(illustrative_var)
+                for value in np.unique(illustrative_var):
+                    selected = np.where(illustrative_var == value)
+                    plt.scatter(X_projected[selected, d1], X_projected[selected, d2], alpha=alpha, label=value)
+                plt.legend()
+
+            # affichage des labels des points
+            if labels is not None:
+                for i,(x,y) in enumerate(X_projected[:,[d1,d2]]):
+                    plt.text(x, y, labels[i],
+                              fontsize='10', ha='center',va='center') 
+                
+            # détermination des limites du graphique
+            boundary = np.max(np.abs(X_projected[:, [d1,d2]])) * 1.1
+            plt.xlim([-boundary,boundary])
+            plt.ylim([-boundary,boundary])
+        
+            # affichage des lignes horizontales et verticales
+            plt.plot([-100, 100], [0, 0], color='grey', ls='--')
+            plt.plot([0, 0], [-100, 100], color='grey', ls='--')
+
+            # nom des axes, avec le pourcentage d'inertie expliqué
+            plt.xlabel('F{} ({}%)'.format(d1+1, round(100*pca.explained_variance_ratio_[d1],1)))
+            plt.ylabel('F{} ({}%)'.format(d2+1, round(100*pca.explained_variance_ratio_[d2],1)))
+
+            plt.title("Projection des individus (sur F{} et F{})".format(d1+1, d2+1))
+            plt.show(block=False)
+            
+def lorenz(df):
+    dep = df.values
+    n = len(dep)
+    lorenz = np.cumsum(np.sort(dep)) / dep.sum()
+    lorenz = np.append([0],lorenz) # La courbe de Lorenz commence à 0
+    
+    AUC = (lorenz.sum() -lorenz[-1]/2 -lorenz[0]/2)/n # Surface sous la courbe de Lorenz. Le premier segment (lorenz[0]) est à moitié en dessous de 0, on le coupe donc en 2, on fait de même pour le dernier segment lorenz[-1] qui est à moitié au dessus de 1.
+    S = 0.5 - AUC # surface entre la première bissectrice et le courbe de Lorenz
+    gini = 2*S
+    
+    return (lorenz, n, gini)
+
+def chi_2(X, Y, df):
+    cont = pd.pivot_table(df[[X, Y]], index=X ,columns=Y, aggfunc=len, margins=True, margins_name="Total")
+
+    tx = cont.loc[:,["Total"]]
+    ty = cont.loc[["Total"],:]
+    n = len(df)
+    indep = tx.dot(ty) / n
+
+    c = cont.fillna(0) # On remplace les valeurs nulles par 0
+    measure = (c-indep)**2/indep
+    xi_n = measure.sum().sum()
+    table = measure/xi_n
+    
+    return (table, c)
+
+def eta_squared(x,y):
+    moyenne_y = y.mean()
+    classes = []
+    for classe in x.unique():
+        yi_classe = y[x==classe]
+        classes.append({'ni': len(yi_classe),
+                        'moyenne_classe': yi_classe.mean()})
+    SCT = sum([(yj-moyenne_y)**2 for yj in y])
+    SCE = sum([c['ni']*(c['moyenne_classe']-moyenne_y)**2 for c in classes])
+    return SCE/SCT
